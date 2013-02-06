5ddfdce604f0dfc2a5b3273d34876b235823b7a4
FritzKatz <fritz_katzfuss@hotmail.com>
2013-02-06 15:10:52 +0100

    Added static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+
   def home
+  	@title = "Home"
+  end
+
+  def about
+  	@title = "About us"
+
   end
 
   def contact
+  	@title = "Contact us"
   end
 end
